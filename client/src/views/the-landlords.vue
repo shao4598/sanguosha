be9f010841a744fd5b132beba5bafb3d2df9c81b
chloe <wentingjiang@outlook.com>
2023-06-11 15:17:27 +0800

    feat: 引入stylelint

--- client/src/views/the-landlords.vue
+++ client/src/views/the-landlords.vue
@@ -951,10 +951,12 @@ export default {
 .table-expand {
   font-size: 0;
 }
+
 .table-expand label {
   width: 90px;
   color: #99a9bf;
 }
+
 .table-expand .el-form-item {
   margin-right: 0;
   margin-bottom: 0;
